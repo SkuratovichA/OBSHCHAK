fa535cfcb8f0f95ca913b778166bed6c1845bae5
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2023-10-25 01:27:15 +0200

    implement a dummy homepage

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,27 +1,54 @@
 'use client'
-import { Button } from '@mui/material'
-import { Layout } from '@OBSHCHAK-UI/components'
-import { ThemeProvider, WebsocketsProvider } from '@OBSHCHAK-UI/hooks'
-import { AppProvider } from '@OBSHCHAK-UI/hooks'
 import React from 'react'
-import { API_PATH, API_VER } from 'app-common'
+import { Button, Typography, Container } from '@mui/material'
+import { motion } from 'framer-motion'
+import styled from '@emotion/styled'
 import { signIn } from 'next-auth/react'
 
-const Home: React.FC = () => {
+const StyledContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`
+
+const LandingPage: React.FC = () => {
   return (
-    <main>
-      <WebsocketsProvider>
-        <ThemeProvider>
-          <AppProvider>
-            <Button variant="contained" color="primary" onClick={() => signIn('google')}>
-              Login with Google
-            </Button>
-            <Layout />
-          </AppProvider>
-        </ThemeProvider>
-      </WebsocketsProvider>
-    </main>
+    <StyledContainer>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+        <Typography variant="h4" gutterBottom>
+          Welcome to Debt Splitter
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          The best app for managing and splitting your expenses.
+        </Typography>
+        <Button variant="contained" color="primary" onClick={() => signIn('google')}>
+          Login with Google
+        </Button>
+      </motion.div>
+    </StyledContainer>
   )
 }
 
-export default Home
+export default LandingPage
+
+
+// const Home: React.FC = () => {
+//   return (
+//     <main>
+//       <WebsocketsProvider>
+//         <ThemeProvider>
+//           <AppProvider>
+//             <Button variant="contained" color="primary" onClick={() => signIn('google')}>
+//               Login with Google
+//             </Button>
+//             <Layout />
+//           </AppProvider>
+//         </ThemeProvider>
+//       </WebsocketsProvider>
+//     </main>
+//   )
+// }
+//
+// export default Home
