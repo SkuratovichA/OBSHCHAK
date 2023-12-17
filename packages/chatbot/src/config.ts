13c965a6704958b09bc2e39977a0910a2dd19aa0
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2023-12-17 22:39:26 +0100

    f chatbot config

--- packages/chatbot/src/config.ts
+++ packages/chatbot/src/config.ts
@@ -1,2 +1,10 @@
+import { hideCriticalString } from 'app-common'
+import { logger } from './loggers'
+import dotenvExpand from 'dotenv-expand'
+import dotenv from 'dotenv'
+
+dotenvExpand(dotenv.config())
+
 export const OPENAI_API_KEY: string = process.env.OPENAI_API_KEY ?? ''
-export const PORT = Number(process.env.PORT) ?? 3030
+
+logger.info(`OPENAI_API_KEY: ${hideCriticalString(OPENAI_API_KEY)}`)
