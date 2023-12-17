7177f18126d302f5277dcfc310c11cdf5805aae3
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2023-12-17 22:57:45 +0100

    remove debug message

--- packages/chatbot/src/chatbot-parts/openai-wrapper/openai-wrapper.ts
+++ packages/chatbot/src/chatbot-parts/openai-wrapper/openai-wrapper.ts
@@ -2,7 +2,6 @@ import OpenAI from 'openai'
 import type { ChatCompletionChunk } from 'openai/resources'
 import type { Stream } from 'openai/streaming'
 
-import { logger } from '../../loggers'
 import { OPENAI_API_KEY } from '../../config'
 import { CompleteFn } from './types'
 import { getOpenAIErrorMessage } from './errors'
@@ -26,7 +25,6 @@ export const complete: CompleteFn = async function* ({ systemPrompt, messages, p
 
   for await (const part of stream) {
     const delta = part.choices[0]?.delta
-    logger.debug(`CHUNK ${JSON.stringify(part.choices[0])}`)
 
     if (delta.content) {
       yield delta.content
