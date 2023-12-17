ed951c59aac62ae15e7b7814e809e69c0aada09f
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2023-12-17 01:05:12 +0100

    add stt

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -5,3 +5,7 @@ export const CLIENT_PORT = process.env.REACT_APP_CLIENT_PORT ?? '3000'
 export const API_VER = 1
 export const API_PATH = `${API_ORIGIN}:${API_PORT}`
 export const CLIENT_PATH = process.env.REACT_APP_CLIENT_PATH ?? `${API_ORIGIN}:${CLIENT_PORT}`
+
+
+export const OPENAI_STT_MODEL = 'whisper-1'
+export const OPENAI_STT_URL = 'https://api.openai.com/v1/audio/transcriptions'
