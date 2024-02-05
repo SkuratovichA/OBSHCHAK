ad2c1dbccd11bcfb218c9fdf148d7d1a481c6222
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2024-02-06 00:49:29 +0100

    upd `Undefine` utility type

--- packages/common/src/types/index.ts
+++ packages/common/src/types/index.ts
@@ -4,5 +4,5 @@ export * from './chatbot'
 
 export type Function<Props = void, Result = void> = (props: Props) => Result
 
-export type Undefine<T> = Record<keyof T, undefined>
+export type Undefine<T> = Partial<Record<keyof T, undefined>>
 
