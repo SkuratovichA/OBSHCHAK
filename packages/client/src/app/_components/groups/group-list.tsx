356c8a632ebc8a2c81ac94ec10901aca636dbe8e
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2024-02-18 14:19:47 +0100

    KAN-18, start doing groups

--- packages/client/src/app/_components/groups/group-list.tsx
+++ packages/client/src/app/_components/groups/group-list.tsx
@@ -3,18 +3,34 @@
 import React from 'react'
 import { GroupItem } from '@OBSHCHAK-UI/app/_components/groups/group-item'
 import { ScrollableBarlessList, ListItemTiltable } from '@OBSHCHAK-UI/app/_components'
+import { useLoading } from '@OBSHCHAK-UI/app/_client-hooks'
+import { isNil } from 'lodash'
 
 
 // group-list ts
 interface GroupsListProps {
-  groups: any[] // FIXME: tmp solution before types are in app-common
+  groups: any[] | undefined // FIXME: tmp solution before types are in app-common
 }
 
-export const GroupsList: React.FC<GroupsListProps> = ({ groups }) =>
-  <ScrollableBarlessList>
-    {groups.map((group) => (
-      <ListItemTiltable key={group.id}>
-        <GroupItem group={group} />
-      </ListItemTiltable>
-    ))}
-  </ScrollableBarlessList>
+export const GroupsList: React.FC<GroupsListProps> = ({ groups }) =>{
+
+  const { isLoading } = useLoading()
+
+  if (isLoading) {
+    return <div>Group list is loading - return a skeleton TODO</div>
+  }
+
+  if (isNil(groups)) {
+    return <div>PROBABLY AN UNHANDLED ERROR</div>
+  }
+
+  return (
+    <ScrollableBarlessList>
+      {groups.map((group) => (
+        <ListItemTiltable key={group.id}>
+          <GroupItem group={group} />
+        </ListItemTiltable>
+      ))}
+    </ScrollableBarlessList>
+  )
+}
