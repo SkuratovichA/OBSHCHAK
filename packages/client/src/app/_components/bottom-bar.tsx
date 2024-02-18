18568afc8dfffaf95e504e00290beb44fd145c91
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2024-02-18 14:09:55 +0100

    improve bottom navigation

--- packages/client/src/app/_components/bottom-bar.tsx
+++ packages/client/src/app/_components/bottom-bar.tsx
@@ -15,7 +15,7 @@ export const BottomBar: React.FC<{}> = () => {
   const pathname = usePathname()
   const router = useRouter()
 
-  const shouldHl = (text: string): boolean => pathname === linkify(text)
+  const shouldHl = (text: string): boolean => pathname.includes(linkify(text))
   const handleRedirect = (path: string) => {
     console.log('redirecting to', path)
     try {
@@ -26,30 +26,27 @@ export const BottomBar: React.FC<{}> = () => {
   }
 
   return (
-    <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
-      <BottomNavigation showLabels style={{ justifyContent: 'center' }}>
-        {[
-          { Friends: { icon: <Group /> } },
-          { Groups: { icon: <Groups /> } },
-          { '': { icon: <AddCircleOutline fontSize="large" color="primary" /> } },
-          { Transactions: { icon: <ListIcon /> } },
-          { Account: { icon: <ProfileIcon /> } },
-        ]
-          .map((a) => Object.entries(a)[0])
-          .map(([text, props]) => ({ ...props, label: text }))
-          .map((props, idx) => (
-            <BottomNavActionHighlighted
-              onClick={() => handleRedirect(linkify(props.label))}
-              // TODO: fucked up constants
-              shouldHighlight={shouldHl(props.label)}
-              highlightColor={'rgba(151,71,255,0.43)'}
-              key={idx}
-              icon={props.icon}
-              label={props.label}
-            />
-          ))}
-      </BottomNavigation>
-    </AppBar>
-
+    <BottomNavigation showLabels style={{ justifyContent: 'center', justifySelf: 'flex-end'}}>
+      {[
+        { Friends: { icon: <Group /> } },
+        { Groups: { icon: <Groups /> } },
+        { 'New Transaction': { icon: <AddCircleOutline fontSize="large" color="primary" /> } },
+        { Transactions: { icon: <ListIcon /> } },
+        { Account: { icon: <ProfileIcon /> } },
+      ]
+        .map((a) => Object.entries(a)[0])
+        .map(([text, props]) => ({ ...props, label: text }))
+        .map((props, idx) => (
+          <BottomNavActionHighlighted
+            onClick={() => handleRedirect(linkify(props.label))}
+            // TODO: fucked up constants
+            shouldHighlight={shouldHl(props.label)}
+            highlightColor={'rgba(151,71,255,0.43)'}
+            key={idx}
+            icon={props.icon}
+            label={props.label}
+          />
+        ))}
+    </BottomNavigation>
   )
 }
