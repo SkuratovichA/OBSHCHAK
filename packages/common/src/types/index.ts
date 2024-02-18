06ff36ba475e7dfd80a266db707cecd975cc3dc1
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2024-02-18 14:18:32 +0100

    add util types (Loadable, WithDimensions, WithSize) for UI comps

--- packages/common/src/types/index.ts
+++ packages/common/src/types/index.ts
@@ -5,3 +5,23 @@ export * from './old'
 
 export type Function<Props = void, Result = void> = (props: Props) => Result
 export type Undefine<T> = Partial<Record<keyof T, undefined>>
+
+export type Loadable<T = {}> = ({
+  isLoading: true
+} & {
+  [P in keyof T]?: T[P]
+}) | ({
+  isLoading?: undefined | false
+} & T) | ({
+  isLoading?: boolean
+} & T)
+
+export type WithSize<SIZE_TYPE = {}, T = {}> = T & { size?: SIZE_TYPE }
+
+export type WithDimensions<T = {}, S extends undefined | number = undefined> = T & ({
+  width: number
+  height: number
+} | {
+  width: S
+  height: S
+})
