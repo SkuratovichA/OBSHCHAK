6b8c7278e8e59c6e8769268dee65f77a54e1b395
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2024-03-20 17:40:01 +0100

    upd mocks, add groups mock

--- packages/common/src/types/group.ts
+++ packages/common/src/types/group.ts
@@ -1,15 +1,17 @@
-import { isObshchakUser } from './obshchakUser'
-import type { ObshchakUser } from './obshchakUser'
-import type { Debt} from './debt';
+import type { ObshchakUser } from './obshchak-user'
+import { isObshchakUser } from './obshchak-user'
+import type { Debt } from './debt'
 import { isDebt } from './debt'
 import type { WithId } from './index'
 import { isWithId } from './index'
+
+export type UserReprForGroup = 'username' | 'id' | 'profileImage'
 
 export type Group = WithId<{
   name: string
   description: string
   creationDate: Date
-  members: ObshchakUser[]
+  members: Array<Pick<ObshchakUser, UserReprForGroup>>
   debts: Debt[]
   iconUrl: string
   admin: ObshchakUser['username']
@@ -19,14 +21,21 @@ export type Group = WithId<{
 export const isGroup = (obj: object): obj is Group =>
   obj &&
   isWithId(obj) &&
-  'name' in obj && typeof obj.name === 'string' &&
-  'description' in obj && typeof obj.description === 'string' &&
-  'creationDate' in obj && obj.creationDate instanceof Date &&
-  'members' in obj && Array.isArray(obj.members) && obj.members.every(isObshchakUser) &&
-  'debts' in obj && Array.isArray(obj.debts) && obj.debts.every(isDebt) &&
-  'iconUrl' in obj && typeof obj.iconUrl === 'string' &&
-  'admin' in obj && typeof obj.admin === 'string' &&
-  'isAdmin' in obj && typeof obj.isAdmin === 'boolean'
-
-
-
+  'name' in obj &&
+  typeof obj.name === 'string' &&
+  'description' in obj &&
+  typeof obj.description === 'string' &&
+  'creationDate' in obj &&
+  obj.creationDate instanceof Date &&
+  'members' in obj &&
+  Array.isArray(obj.members) &&
+  obj.members.every(isObshchakUser) &&
+  'debts' in obj &&
+  Array.isArray(obj.debts) &&
+  obj.debts.every(isDebt) &&
+  'iconUrl' in obj &&
+  typeof obj.iconUrl === 'string' &&
+  'admin' in obj &&
+  typeof obj.admin === 'string' &&
+  'isAdmin' in obj &&
+  typeof obj.isAdmin === 'boolean'
