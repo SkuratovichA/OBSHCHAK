24fea68d2a3fb6b2701e458eae7c969692601f4b
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2024-03-21 01:48:04 +0100

    upd backend routes for groups

---
+++ packages/client/src/app/api/groups/add/route.ts
@@ -0,0 +1,20 @@
+import type { NextRequest } from 'next/server'
+import { deserializeGroup } from '@OBSHCHAK-UI/app/api/groups/utils'
+import { isGroupsAddRequestBody } from '@OBSHCHAK-UI/app/api/groups/utils'
+import { arrayToIdMap, groupsMock, updateGroupsMock } from 'app-common'
+
+export async function POST(request: NextRequest) {
+  const body = await request.json()
+  if (!isGroupsAddRequestBody(body)) {
+    return Response.json(undefined, { status: 400 })
+  }
+
+  const newGroups = body.groups
+  // const res = await fetch(`/v${API_VER}/groups`)
+  // const data = await res.json()
+  const groupsMap = arrayToIdMap(
+    updateGroupsMock([...groupsMock(), ...newGroups.map(deserializeGroup)])
+  )
+
+  return Response.json(groupsMap, { status: 200 })
+}
--- packages/client/src/app/api/groups/route.ts
+++ packages/client/src/app/api/groups/route.ts
@@ -1,31 +1,34 @@
 import type { NextRequest } from 'next/server'
-import { isArray } from 'lodash'
+import type { ObshchakUser } from 'app-common'
+import { arrayToIdMap, groupsMock } from 'app-common'
+import type { GroupsResponse} from '@OBSHCHAK-UI/app/api/groups/utils';
+import { serializeGroup } from '@OBSHCHAK-UI/app/api/groups/utils'
+import { isGroupsRequestBody } from '@OBSHCHAK-UI/app/api/groups/utils'
 
 export type GroupsSearchParams = {
-  usernames: string[]
+  usernames: ObshchakUser['username']
 } & Partial<{
   page: number
   limit: number
 }>
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type GroupsSearchResponse = any[]
-
-const isGroupSearchParams = (obj: object): obj is GroupsSearchParams =>
-  obj && 'usernames' in obj && isArray(obj.usernames)
-
 export async function POST(request: NextRequest) {
-  console.log('Hello in group fetching')
   const body: GroupsSearchParams = await request.json()
 
-  if (!isGroupSearchParams(body)) {
+  if (!isGroupsRequestBody(body)) {
     return Response.json(undefined, { status: 400 })
   }
-  console.log('group fetching body:', body)
 
   // const res = await fetch(`/v${API_VER}/groups`)
   // const data = await res.json()
-  const data: GroupsSearchResponse = []
-
+  const data: GroupsResponse = arrayToIdMap(
+    groupsMock()
+      .filter(group =>
+        !body.usernames ||
+        // check if some of the usernames are in the array of group.members({usernames})
+        body.usernames.some(username => group.members.some(member => member.username === username)),
+      )
+      .map(serializeGroup),
+  )
   return Response.json(data, { status: 200 })
 }
