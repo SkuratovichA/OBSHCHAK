7997acdc77d12fcc0e22035aa8c3af5aa3798a5e
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2024-03-24 02:31:38 +0100

    mixin some mixins

---
+++ packages/client/src/app/_components/atoms/mixins.tsx
@@ -0,0 +1,9 @@
+import type React from 'react'
+
+export const centered: React.CSSProperties = {
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+  alignContent: 'center'
+}
+
