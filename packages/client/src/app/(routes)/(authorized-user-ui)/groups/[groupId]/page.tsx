5c777dff6e638644c6b969cf71054518a257c715
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2024-03-24 02:44:36 +0100

    integrate messy group view components

--- packages/client/src/app/(routes)/(authorized-user-ui)/groups/[groupId]/page.tsx
+++ packages/client/src/app/(routes)/(authorized-user-ui)/groups/[groupId]/page.tsx
@@ -1,15 +1,44 @@
+'use client'
 
-const GroupsSkeleton: React.FC = () => {
-  // use mui skeleton to create a page with the photo, some text etc.
+import React from 'react'
+import { nextEndpointsMap } from 'app-common'
+import { useSwr } from '@OBSHCHAK-UI/app/_client-hooks'
+import type { GroupsRequestBody, GroupsResponse } from '@OBSHCHAK-UI/app/api/groups/utils'
+import { deserializeGroup } from '@OBSHCHAK-UI/app/api/groups/utils'
+import {
+  GroupView,
+  GroupViewSkeleton,
+  ScrollableBarlessList,
+} from '@OBSHCHAK-UI/app/_components'
+import { match, P } from 'ts-pattern'
 
-  return <div>
-    Groups are Loading
-  </div>
+type PageProps = {
+  params: {
+    groupId: string
+  }
 }
+const Page: React.FC<PageProps> = ({ params: { groupId } }) => {
 
-const Page: React.FC = () => {
-  return <GroupsSkeleton />
+  const { data: groups, isLoading } = useSwr<GroupsRequestBody, GroupsResponse>(
+    nextEndpointsMap.GROUPS(),
+    { groupId, usernames: [] },
+  )
+
+  return (
+    <ScrollableBarlessList>
+      {match([isLoading, groups])
+        .returnType<React.ReactNode>()
+        .with([false, P.select('groups', P.not(P.nullish))], ({ groups }) =>
+          <GroupView group={
+            deserializeGroup(groups[groupId])
+          } />,
+        )
+        .otherwise(() =>
+          <GroupViewSkeleton />,
+        )
+      }
+    </ScrollableBarlessList>
+  )
 }
-
 
 export default Page
